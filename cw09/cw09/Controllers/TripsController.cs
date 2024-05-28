49f26ee45fdb713cf09ba67852af394b17f1e5c9
Michał Woliński <s29239@pjwstk.edu.pl>
2024-05-28 13:12:12 +0200

    end of class

---
+++ cw09/cw09/Controllers/TripsController.cs
@@ -0,0 +1,37 @@
+﻿using cw09.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace cw09.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+    
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips.Select(e => new
+            {
+                Name = e.Name,
+                Countries = e.IdCountries.Select(c => new
+                {
+                    Name = c.Name
+                })
+            })
+            .ToListAsync();
+
+        var tripsIcnlude = await _context.Trips
+            .Include(e => e.IdCountries)
+            .ToListAsync();
+
+        return Ok(trips);
+    }
+}
